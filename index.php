06fe17438c18fbb092d0297e8b490466067d237b
yosoymitxel <yosoymitxel@gmail.com>
2023-06-02 16:07:34 -0400

    Comienzo de añadir documentación y correcciones

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
     require __DIR__.'/vendor/autoload.php';
     use ScPHPLibrary\ScPHP;
 
-    $variable = 'Hola, mundo!';
+
+
+    $variable = 'Hoañelt0\'4 wi44tla, mundo!';
     $sc = new ScPHP();
-    $sc->dev_var_dump($variable);
+    $sc->dev_activar_depurar_global(true);
+
+    echo $sc->url_str_a_url_amigable($variable);
 
 ?>
